11b24311a89fa699f0b888ebe3d3fea7fa7c6d90
Хузин Ильдар <ildar.khuzin75@mail.ru>
2025-01-11 18:49:53 +0300

    настроил swagger для resume-generator

---
+++ resume-generator/src/main/java/webapp/resumegenerator/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+
+
+package webapp.resumegenerator.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Generator API")
+                        .version("1.0")
+                        .description("Описание API генератора резюме"));
+    }
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
+
